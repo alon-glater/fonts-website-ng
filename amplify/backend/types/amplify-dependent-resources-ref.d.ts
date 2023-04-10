edb422e04db69e965937ff92826ec18d928ef0da
Alon <alon.glater@protonmail.com>
2023-04-10 14:04:25 +0300

    Add storage in backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3fontswebsitengstorage3bf2fba0": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
